9a33f2fc04f7d3ae637c585147dcb97d5cb9f74c
testoun <testoun@proton.me>
2026-01-07 14:49:58 +0100

    trying to fix style issue

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,7 +2,12 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   darkMode: "class",
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+    "../../**/**/*.{js,ts,jsx,tsx}",
+    "../../",
+  ],
   theme: {
     extend: {},
   },
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     port: 3000,
     host: true,
   },
+  base: "./",
   plugins: [
     react({
       babel: {
