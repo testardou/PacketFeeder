88b3b8d5468a89be183a60d83a41faf4186f8143
testoun <testoun@proton.me>
2026-01-07 15:36:30 +0100

    trying to fix style issue

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,7 +2,11 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
